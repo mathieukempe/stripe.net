47187347bb800494c683564fee121ea1646e8441
mathieukempe <mathieukempe@gmail.com>
2015-06-02 19:06:34 +1000

    Cleanup

--- src/Stripe/Infrastructure/FileInfoExtensions.cs
+++ src/Stripe/Infrastructure/FileInfoExtensions.cs
@@ -7,7 +7,7 @@ namespace Stripe
     /// <summary>
     /// Extension methods for <see cref="System.IO.FileInfo"/>. From http://www.paraesthesia.com/archive/2009/12/16/posting-multipartform-data-using-.net-webrequest.aspx/
     /// </summary>
-    public static class FileInfoExtensions
+    public static class FileStreamExtensions
     {
         /// <summary>
         /// Template for a file item in multipart/form-data format.
@@ -21,17 +21,7 @@ namespace Stripe
         /// <param name="stream">The stream to which the file should be written.</param>
         /// <param name="mimeBoundary">The MIME multipart form boundary string.</param>
         /// <param name="mimeType">The MIME type of the file.</param>
-        /// <param name="formKey">The name of the form parameter corresponding to the file upload.</param>
-        /// <exception cref="System.ArgumentNullException">
-        /// Thrown if any parameter is <see langword="null" />.
-        /// </exception>
-        /// <exception cref="System.ArgumentException">
-        /// Thrown if <paramref name="mimeBoundary" />, <paramref name="mimeType" />,
-        /// or <paramref name="formKey" /> is empty.
-        /// </exception>
-        /// <exception cref="System.IO.FileNotFoundException">
-        /// Thrown if <paramref name="file" /> does not exist.
-        /// </exception>
+        /// <param name="formKey">The name of the form parameter corresponding to the file upload.</param>        
         public static void WriteMultipartFormData(this FileStream fileStream, Stream stream, string mimeBoundary, string mimeType, string formKey)
         {
             string header = String.Format(HeaderTemplate, mimeBoundary, formKey, fileStream.Name, mimeType);
