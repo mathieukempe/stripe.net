fcaee30cc53c1f8b91de603848af4b65614215c6
carlosboeing <carlosboeing@gmail.com>
2015-08-07 12:24:35 +1000

    Fixed StripeDisputeUpdateOptions json properties

--- src/Stripe/Services/Disputes/StripeDisputeUpdateOptions.cs
+++ src/Stripe/Services/Disputes/StripeDisputeUpdateOptions.cs
@@ -4,86 +4,86 @@ using System.Linq;
 using System.Text;
 using Newtonsoft.Json;
 
-namespace Stripe.Services.Disputes
+namespace Stripe
 {
     public class StripeDisputeUpdateOptions
     {
-        [JsonProperty("product_description")]
+        [JsonProperty("evidence[product_description]")]
         public string ProductDescription { get; set; }
 
-        [JsonProperty("customer_name")]
+        [JsonProperty("evidence[customer_name]")]
         public string CustomerName { get; set; }
 
-        [JsonProperty("customer_email_address")]
+        [JsonProperty("evidence[customer_email_address]")]
         public string CustomerEmailAddress { get; set; }
 
-        [JsonProperty("customer_purchase_ip")]
+        [JsonProperty("evidence[customer_purchase_ip]")]
         public string CustomerPurchaseIp { get; set; }
 
-        [JsonProperty("customer_signature")]
+        [JsonProperty("evidence[customer_signature]")]
         public string CustomerSignature { get; set; }
 
-        [JsonProperty("billing_address")]
+        [JsonProperty("evidence[billing_address]")]
         public string BillingAddress { get; set; }
 
-        [JsonProperty("receipt")]
+        [JsonProperty("evidence[receipt]")]
         public string Receipt { get; set; }
 
-        [JsonProperty("shipping_address")]
+        [JsonProperty("evidence[shipping_address]")]
         public string ShippingAddress { get; set; }
 
-        [JsonProperty("shipping_date")]
+        [JsonProperty("evidence[shipping_date]")]
         public string ShippingDate { get; set; }
 
-        [JsonProperty("shipping_carrier")]
+        [JsonProperty("evidence[shipping_carrier]")]
         public string ShippingCarrier { get; set; }
 
-        [JsonProperty("shipping_tracking_number")]
+        [JsonProperty("evidence[shipping_tracking_number]")]
         public string ShippingTrackingNumber { get; set; }
 
-        [JsonProperty("shipping_documentation")]
+        [JsonProperty("evidence[shipping_documentation]")]
         public string ShippingDocumentation { get; set; }
 
-        [JsonProperty("access_activity_log")]
+        [JsonProperty("evidence[access_activity_log]")]
         public string AccessActivityLog { get; set; }
 
-        [JsonProperty("service_date")]
+        [JsonProperty("evidence[service_date]")]
         public string ServiceDate { get; set; }
 
-        [JsonProperty("service_documentation")]
+        [JsonProperty("evidence[service_documentation]")]
         public string ServiceDocumentation { get; set; }
 
-        [JsonProperty("duplicate_charge_id")]
+        [JsonProperty("evidence[duplicate_charge_id]")]
         public string DuplicateChargeId { get; set; }
 
-        [JsonProperty("duplicate_charge_explanation")]
+        [JsonProperty("evidence[duplicate_charge_explanation]")]
         public string DuplicateChargeExplanation { get; set; }
 
-        [JsonProperty("refund_policy")]
+        [JsonProperty("evidence[refund_policy]")]
         public string RefundPolicy { get; set; }
 
-        [JsonProperty("refund_policy_disclosure")]
+        [JsonProperty("evidence[refund_policy_disclosure]")]
         public string RefundPolicyDisclosure { get; set; }
 
-        [JsonProperty("refund_refusal_explanation")]
+        [JsonProperty("evidence[refund_refusal_explanation]")]
         public string RefundRefusalExplanation { get; set; }
 
-        [JsonProperty("cancellation_policy")]
+        [JsonProperty("evidence[cancellation_policy]")]
         public string CancellationPolicy { get; set; }
 
-        [JsonProperty("cancellation_policy_disclosure")]
+        [JsonProperty("evidence[cancellation_policy_disclosure]")]
         public string CancellationPolicyDisclosure { get; set; }
 
-        [JsonProperty("cancellation_rebuttal")]
+        [JsonProperty("evidence[cancellation_rebuttal]")]
         public string CancellationRebuttal { get; set; }
 
-        [JsonProperty("customer_communication")]
+        [JsonProperty("evidence[customer_communication]")]
         public string CustomerCommunication { get; set; }
 
-        [JsonProperty("uncategorized_file")]
+        [JsonProperty("evidence[uncategorized_file]")]
         public string UncategorizedFile { get; set; }
 
-        [JsonProperty("uncategorized_text")]
+        [JsonProperty("evidence[uncategorized_text]")]
         public string UncategorizedText { get; set; }
 
     }
