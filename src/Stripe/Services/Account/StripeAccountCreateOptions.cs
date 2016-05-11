a0fc459596cef6cd7515195cba4960a17b324132
Mathieu Kempe <mathieukempe@gmail.com>
2016-05-11 10:03:42 +1000

    Added managed account properties for Japan

--- src/Stripe/Services/Account/StripeAccountCreateOptions.cs
+++ src/Stripe/Services/Account/StripeAccountCreateOptions.cs
@@ -34,23 +34,62 @@ namespace Stripe
         [JsonProperty("legal_entity[type]")]
         public string LegalEntityType { get; set; }
 
+        [JsonProperty("legal_entity[gender]")]
+        public string LegalEntityGender { get; set; }
+
         [JsonProperty("legal_entity[business_name]")]
         public string LegalEntityBusinessName { get; set; }
+
+        [JsonProperty("legal_entity[business_name_kana]")]
+        public string LegalEntityBusinessNameKana { get; set; }
+
+        [JsonProperty("legal_entity[business_name_kanji]")]
+        public string LegalEntityBusinessNameKanji { get; set; }
 
         [JsonProperty("legal_entity[address][line1]")]
         public string LegalEntityAddressLine1 { get; set; }
 
+        [JsonProperty("legal_entity[address_kana][line1]")]
+        public string LegalEntityAddressKanaLine1 { get; set; }
+
+        [JsonProperty("legal_entity[address_kanji][line1]")]
+        public string LegalEntityAddressKanjiLine1 { get; set; }
+
         [JsonProperty("legal_entity[address][line2]")]
         public string LegalEntityAddressLine2 { get; set; }
+
+        [JsonProperty("legal_entity[address_kanji][town]")]
+        public string LegalEntityAddressKanjiTown { get; set; }
+
+        [JsonProperty("legal_entity[address_kana][town]")]
+        public string LegalEntityAddressKanaTown { get; set; }
 
         [JsonProperty("legal_entity[address][city]")]
         public string LegalEntityAddressCity { get; set; }
 
+        [JsonProperty("legal_entity[address_kana][city]")]
+        public string LegalEntityAddressKanjiCity { get; set; }
+
+        [JsonProperty("legal_entity[address_kanji][city]")]
+        public string LegalEntityAddressKanaCity { get; set; }
+
         [JsonProperty("legal_entity[address][state]")]
         public string LegalEntityAddressState { get; set; }
 
+        [JsonProperty("legal_entity[address_kanji][state]")]
+        public string LegalEntityAddressKanjiState { get; set; }
+
+        [JsonProperty("legal_entity[address_kana][state]")]
+        public string LegalEntityAddressKanaState { get; set; }
+
         [JsonProperty("legal_entity[address][postal_code]")]
         public string LegalEntityAddressPostalCode { get; set; }
+
+        [JsonProperty("legal_entity[address_kana][postal_code]")]
+        public string LegalEntityAddressKanaPostalCode { get; set; }
+
+        [JsonProperty("legal_entity[address_kanji][postal_code]")]
+        public string LegalEntityAddressKanjiPostalCode { get; set; }
 
         [JsonProperty("legal_entity[address][country]")]
         public string LegalEntityAddressCountry { get; set; }
@@ -67,8 +106,20 @@ namespace Stripe
         [JsonProperty("legal_entity[first_name]")]
         public string LegalEntityFirstName { get; set; }
 
+        [JsonProperty("legal_entity[first_name_kana]")]
+        public string LegalEntityFirstNameKana { get; set; }
+
+        [JsonProperty("legal_entity[first_name_kanji]")]
+        public string LegalEntityFirstNameKanji { get; set; }
+
         [JsonProperty("legal_entity[last_name]")]
         public string LegalEntityLastName { get; set; }
+
+        [JsonProperty("legal_entity[last_name_kana]")]
+        public string LegalEntityLastNameKana { get; set; }
+
+        [JsonProperty("legal_entity[last_name_kanji]")]
+        public string LegalEntityLastNameKanji { get; set; }
 
         [JsonProperty("legal_entity[ssn_last_4]")]
         public string SSN4 { get; set; }
@@ -97,17 +148,47 @@ namespace Stripe
         [JsonProperty("legal_entity[personal_address][line1]")]
         public string PersonalAddressLine1 { get; set; }
 
+        [JsonProperty("legal_entity[personal_address_kanji][line1]")]
+        public string PersonalAddressKanjiLine1 { get; set; }
+
+        [JsonProperty("legal_entity[personal_address_kana][line1]")]
+        public string PersonalAddressKanaLine1 { get; set; }
+
         [JsonProperty("legal_entity[personal_address][line2]")]
         public string PersonalAddressLine2 { get; set; }
+
+        [JsonProperty("legal_entity[personal_address_kanji][town]")]
+        public string PersonalAddressKanjiTown { get; set; }
+
+        [JsonProperty("legal_entity[personal_address_kana][town]")]
+        public string PersonalAddressKanaTown { get; set; }
 
         [JsonProperty("legal_entity[personal_address][city]")]
         public string PersonalAddressCity { get; set; }
 
+        [JsonProperty("legal_entity[personal_address_kanji][city]")]
+        public string PersonalAddressKanjiCity { get; set; }
+
+        [JsonProperty("legal_entity[personal_address_kana][city]")]
+        public string PersonalAddressKanaCity { get; set; }
+
         [JsonProperty("legal_entity[personal_address][state]")]
         public string PersonalAddressState { get; set; }
 
+        [JsonProperty("legal_entity[personal_address_kanji][state]")]
+        public string PersonalAddressKanjiState { get; set; }
+
+        [JsonProperty("legal_entity[personal_address_kana][state]")]
+        public string PersonalAddressKanaState { get; set; }
+
         [JsonProperty("legal_entity[personal_address][postal_code]")]
         public string PersonalAddressPostalCode { get; set; }
+
+        [JsonProperty("legal_entity[personal_address_kanji][postal_code]")]
+        public string PersonalAddressKanjiPostalCode { get; set; }
+
+        [JsonProperty("legal_entity[personal_address_kana][postal_code]")]
+        public string PersonalAddressKanaPostalCode { get; set; }
 
         [JsonProperty("legal_entity[personal_address][country]")]
         public string PersonalAddressCountry { get; set; }
